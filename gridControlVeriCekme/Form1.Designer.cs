079d57292053c2c3beeb53a1b44ecf4d775d334f
İbrahim KARMAZ <107946057+ibrahimkarmaz@users.noreply.github.com>
2022-07-09 17:07:30 +0300

    GridControl Üzerinden veri çekme (2. yöntem)
    İki yöntemde bu olayın içine yazıldı FocusedRowChange
    
    MY Yöntemi ile;
    DataRow aaa=GV.GetDataRow(GV.FocusedRowHandle)
    Çekerken
    x.text=aaa[0].ToString() gibi
    
    Araştırma sonucu yöntem ile ;
    GV=GridView1
    GV.GetRowCellValue(GV.FocusedRowHandle,gv.columns[0])

---
+++ gridControlVeriCekme/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace gridControlVeriCekme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 0);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(571, 402);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsView.ShowFooter = true;
+            this.gridView1.OptionsView.ShowGroupPanel = false;
+            this.gridView1.FocusedRowChanged += new DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventHandler(this.gridView1_FocusedRowChanged);
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Location = new System.Drawing.Point(12, 372);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(144, 18);
+            this.labelControl1.TabIndex = 1;
+            this.labelControl1.Text = "SEÇİLEN BİLGİLER";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Location = new System.Drawing.Point(415, 372);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(144, 18);
+            this.labelControl2.TabIndex = 2;
+            this.labelControl2.Text = "SEÇİLEN BİLGİLER";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 402);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.gridControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+    }
+}
+
---
+++ gridControlVeriCekme/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gridControlVeriCekme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SCAEntities dataBase = new SCAEntities();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gridControl1.DataSource = (from ilce in dataBase.ilcelers
+                                       join il in dataBase.illers
+                                       on ilce.sehir equals il.id
+                                       select new
+                                       {
+                                           İL = il.sehir,
+                                           İLÇE = ilce.ilce
+                                       }).ToList();
+
+            gridView1.OptionsBehavior.ReadOnly = true;
+            gridView1.OptionsBehavior.Editable = false;
+        }
+
+        private void gridView1_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
+        {//İMLECİN ODAKLANDIĞI SATIR DEĞİŞİNCE NE OLSUN ?
+            //1.YÖNTEM GetRowCellValue ile satır ve alan adını belirtip veri alabiriz.
+            labelControl1.Text = "SEÇİLİ ALAN:" + gridView1.GetRowCellValue(gridView1.FocusedRowHandle, gridView1.Columns[0])+"=>"+ gridView1.GetRowCellValue(gridView1.FocusedRowHandle, gridView1.Columns[1]);
+            DataRow dr = gridView1.GetDataRow(gridView1.FocusedRowHandle);
+            if (dr!=null)//İLK ÇALIŞMADA BOŞ DEĞER ÇEKMEYE ÇALIŞIYOR İF İFADESİ KULLANILDI
+            {
+                labelControl2.Text = dr[0].ToString();
+            }
+        }
+    }
+}
