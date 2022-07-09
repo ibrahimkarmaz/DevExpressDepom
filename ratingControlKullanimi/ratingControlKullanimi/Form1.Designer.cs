08a4cfe58da3ab0f1619918e4c27a53af34fecee
İbrahim KARMAZ <107946057+ibrahimkarmaz@users.noreply.github.com>
2022-07-09 18:41:12 +0300

    Rating Control Kullanımı
    Rating Controlun nasıl kullanacağını hakkında kısa bilgi.

---
+++ ratingControlKullanimi/ratingControlKullanimi/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace ratingControlKullanimi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.ratingControl1 = new DevExpress.XtraEditors.RatingControl();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ratingControl1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 0);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(662, 446);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsView.ShowFooter = true;
+            this.gridView1.OptionsView.ShowGroupPanel = false;
+            this.gridView1.FocusedRowChanged += new DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventHandler(this.gridView1_FocusedRowChanged);
+            // 
+            // ratingControl1
+            // 
+            this.ratingControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ratingControl1.Location = new System.Drawing.Point(12, 418);
+            this.ratingControl1.Name = "ratingControl1";
+            this.ratingControl1.Rating = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.ratingControl1.Size = new System.Drawing.Size(92, 16);
+            this.ratingControl1.TabIndex = 1;
+            this.ratingControl1.Text = "ratingControl1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 446);
+            this.Controls.Add(this.ratingControl1);
+            this.Controls.Add(this.gridControl1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ratingControl1.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraEditors.RatingControl ratingControl1;
+    }
+}
+
---
+++ ratingControlKullanimi/ratingControlKullanimi/Form1.cs
@@ -0,0 +1,48 @@
+﻿using DevExpress.Skins;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ratingControlKullanimi
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Db_FilmArsivEntities dataBase = new Db_FilmArsivEntities();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*NOT:
+             *YILDIZLARI FARKLI BİR SİMGE İLE DEĞİŞTİRME KONUSUNU ARAŞTIR
+             *YILDIZLARIN RENGİNİ DEĞİŞTİRME İLE İLGİLİ KONUYU ARAŞTIR
+             */
+
+            gridView1.OptionsBehavior.ReadOnly = true;
+            gridView1.OptionsBehavior.Editable = false;
+            gridControl1.DataSource = dataBase.Tbl_Filmler.ToList();
+
+        }
+
+        private void gridView1_FocusedRowChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
+        {
+           double deger=(Convert.ToInt32(gridView1.GetRowCellValue(gridView1.FocusedRowHandle,gridView1.Columns[3])));
+            if (deger>0 && deger<=2)
+                ratingControl1.EditValue = 1;
+            else if (deger > 2 && deger <= 4)
+                ratingControl1.EditValue = 2;
+            else if (deger > 4 && deger <= 6)
+                ratingControl1.EditValue = 3;
+            else if (deger > 6 && deger <= 8)
+                ratingControl1.EditValue = 4;
+            else if (deger > 8 && deger <= 10)
+                ratingControl1.EditValue = 5;
+        }
+    }
+}
