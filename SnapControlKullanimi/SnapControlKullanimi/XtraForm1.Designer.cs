f35759444a73aae7d1d9909bed14ef37226b0eee
İbrahim KARMAZ <107946057+ibrahimkarmaz@users.noreply.github.com>
2022-08-06 17:21:23 +0300

    SnapControl Kullanimi
    Veritabani alanlar üzerinde işlemler yapılabildiği alandır.
    Sorun: Spx formatında verileri kaydettiğimde veritabanında değişiklik olduğunda bu veriler yenilenmiyor.

---
+++ SnapControlKullanimi/SnapControlKullanimi/XtraForm1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace SnapControlKullanimi
+{
+    partial class XtraForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.snapControl1 = new DevExpress.Snap.SnapControl();
+            this.snapDockManager1 = new DevExpress.Snap.Extensions.SnapDockManager(this.components);
+            this.fieldListDockPanel1 = new DevExpress.Snap.Extensions.UI.FieldListDockPanel();
+            this.fieldListDockPanel1_Container = new DevExpress.XtraBars.Docking.ControlContainer();
+            this.reportExplorerDockPanel1 = new DevExpress.Snap.Extensions.UI.ReportExplorerDockPanel();
+            this.reportExplorerDockPanel1_Container = new DevExpress.XtraBars.Docking.ControlContainer();
+            this.snapDocumentManager1 = new DevExpress.Snap.Extensions.SnapDocumentManager(this.components);
+            this.noDocumentsView1 = new DevExpress.XtraBars.Docking2010.Views.NoDocuments.NoDocumentsView(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.snapDockManager1)).BeginInit();
+            this.fieldListDockPanel1.SuspendLayout();
+            this.reportExplorerDockPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.snapDocumentManager1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.noDocumentsView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // snapControl1
+            // 
+            this.snapControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.snapControl1.Location = new System.Drawing.Point(0, 0);
+            this.snapControl1.Name = "snapControl1";
+            this.snapControl1.Options.DocumentCapabilities.TrackChanges = DevExpress.XtraRichEdit.DocumentCapability.Disabled;
+            this.snapControl1.Options.SnapMailMergeVisualOptions.DataSourceName = null;
+            this.snapControl1.Size = new System.Drawing.Size(736, 479);
+            this.snapControl1.TabIndex = 0;
+            this.snapControl1.Text = "snapControl1";
+            // 
+            // snapDockManager1
+            // 
+            this.snapDockManager1.Form = this;
+            this.snapDockManager1.HiddenPanels.AddRange(new DevExpress.XtraBars.Docking.DockPanel[] {
+            this.reportExplorerDockPanel1,
+            this.fieldListDockPanel1});
+            this.snapDockManager1.SnapControl = this.snapControl1;
+            this.snapDockManager1.TopZIndexControls.AddRange(new string[] {
+            "DevExpress.XtraBars.BarDockControl",
+            "DevExpress.XtraBars.StandaloneBarDockControl",
+            "System.Windows.Forms.MenuStrip",
+            "System.Windows.Forms.StatusStrip",
+            "System.Windows.Forms.StatusBar",
+            "DevExpress.XtraBars.Ribbon.RibbonStatusBar",
+            "DevExpress.XtraBars.Ribbon.RibbonControl",
+            "DevExpress.XtraBars.Navigation.OfficeNavigationBar",
+            "DevExpress.XtraBars.Navigation.TileNavPane",
+            "DevExpress.XtraBars.TabFormControl",
+            "DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormControl",
+            "DevExpress.XtraBars.ToolbarForm.ToolbarFormControl"});
+            // 
+            // fieldListDockPanel1
+            // 
+            this.fieldListDockPanel1.Controls.Add(this.fieldListDockPanel1_Container);
+            this.fieldListDockPanel1.Dock = DevExpress.XtraBars.Docking.DockingStyle.Right;
+            this.fieldListDockPanel1.ID = new System.Guid("bfa8f835-0638-47d8-854d-bed7c2f6a266");
+            this.fieldListDockPanel1.Location = new System.Drawing.Point(536, 0);
+            this.fieldListDockPanel1.Name = "fieldListDockPanel1";
+            this.fieldListDockPanel1.OriginalSize = new System.Drawing.Size(200, 200);
+            this.fieldListDockPanel1.SavedDock = DevExpress.XtraBars.Docking.DockingStyle.Right;
+            this.fieldListDockPanel1.SavedIndex = 0;
+            this.fieldListDockPanel1.Size = new System.Drawing.Size(200, 479);
+            this.fieldListDockPanel1.SnapControl = this.snapControl1;
+            this.fieldListDockPanel1.Visibility = DevExpress.XtraBars.Docking.DockVisibility.Hidden;
+            // 
+            // fieldListDockPanel1_Container
+            // 
+            this.fieldListDockPanel1_Container.Location = new System.Drawing.Point(4, 26);
+            this.fieldListDockPanel1_Container.Name = "fieldListDockPanel1_Container";
+            this.fieldListDockPanel1_Container.Size = new System.Drawing.Size(193, 450);
+            this.fieldListDockPanel1_Container.TabIndex = 0;
+            // 
+            // reportExplorerDockPanel1
+            // 
+            this.reportExplorerDockPanel1.Controls.Add(this.reportExplorerDockPanel1_Container);
+            this.reportExplorerDockPanel1.Dock = DevExpress.XtraBars.Docking.DockingStyle.Fill;
+            this.reportExplorerDockPanel1.ID = new System.Guid("cc1e5dcb-b442-4619-aadb-31358ea410ac");
+            this.reportExplorerDockPanel1.Location = new System.Drawing.Point(0, 240);
+            this.reportExplorerDockPanel1.Name = "reportExplorerDockPanel1";
+            this.reportExplorerDockPanel1.OriginalSize = new System.Drawing.Size(98, 200);
+            this.reportExplorerDockPanel1.SavedDock = DevExpress.XtraBars.Docking.DockingStyle.Fill;
+            this.reportExplorerDockPanel1.SavedIndex = 1;
+            this.reportExplorerDockPanel1.SavedParent = this.fieldListDockPanel1;
+            this.reportExplorerDockPanel1.Size = new System.Drawing.Size(200, 239);
+            this.reportExplorerDockPanel1.SnapControl = this.snapControl1;
+            this.reportExplorerDockPanel1.Visibility = DevExpress.XtraBars.Docking.DockVisibility.Hidden;
+            // 
+            // reportExplorerDockPanel1_Container
+            // 
+            this.reportExplorerDockPanel1_Container.Location = new System.Drawing.Point(4, 26);
+            this.reportExplorerDockPanel1_Container.Name = "reportExplorerDockPanel1_Container";
+            this.reportExplorerDockPanel1_Container.Size = new System.Drawing.Size(193, 210);
+            this.reportExplorerDockPanel1_Container.TabIndex = 0;
+            // 
+            // snapDocumentManager1
+            // 
+            this.snapDocumentManager1.ClientControl = this.snapControl1;
+            this.snapDocumentManager1.View = this.noDocumentsView1;
+            this.snapDocumentManager1.ViewCollection.AddRange(new DevExpress.XtraBars.Docking2010.Views.BaseView[] {
+            this.noDocumentsView1});
+            // 
+            // XtraForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 479);
+            this.Controls.Add(this.snapControl1);
+            this.Name = "XtraForm1";
+            this.Text = "XtraForm1";
+            this.Load += new System.EventHandler(this.XtraForm1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.snapDockManager1)).EndInit();
+            this.fieldListDockPanel1.ResumeLayout(false);
+            this.reportExplorerDockPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.snapDocumentManager1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.noDocumentsView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.Snap.SnapControl snapControl1;
+        private DevExpress.Snap.Extensions.SnapDockManager snapDockManager1;
+        private DevExpress.Snap.Extensions.UI.FieldListDockPanel fieldListDockPanel1;
+        private DevExpress.XtraBars.Docking.ControlContainer fieldListDockPanel1_Container;
+        private DevExpress.Snap.Extensions.UI.ReportExplorerDockPanel reportExplorerDockPanel1;
+        private DevExpress.XtraBars.Docking.ControlContainer reportExplorerDockPanel1_Container;
+        private DevExpress.Snap.Extensions.SnapDocumentManager snapDocumentManager1;
+        private DevExpress.XtraBars.Docking2010.Views.NoDocuments.NoDocumentsView noDocumentsView1;
+    }
+}
---
+++ SnapControlKullanimi/SnapControlKullanimi/XtraForm1.cs
@@ -0,0 +1,28 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.Snap;
+using DevExpress.Snap.Core.API;
+
+namespace SnapControlKullanimi
+{
+    public partial class XtraForm1 : DevExpress.XtraEditors.XtraForm
+    {
+        public XtraForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void XtraForm1_Load(object sender, EventArgs e)
+        {
+            snapControl1.LoadDocument(@"C:\Users\ibrah\OneDrive\Masaüstü\RaporDeneme.snx", SnapDocumentFormat.Snap);
+        }
+    }
+}
