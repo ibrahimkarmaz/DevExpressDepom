2b032da8d203940e193f847af1048d542e7eab26
İbrahim KARMAZ <107946057+ibrahimkarmaz@users.noreply.github.com>
2022-08-08 22:28:45 +0300

    Wizard ve ExportTo işlemleri
    Wizard kullanımı
    gridview üzerinde renk değiştirme işlemleri
    ExportToPdf vs -->burada bir adrese kaydetme
    
    Bir dosya içinde öğe arama bunu exportto ile kaydettiğimiz dosyayı açma.

---
+++ ExportToXKullanimi/ExportToXKullanimi/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace ExportToXKullanimi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.DataAccess.Sql.SelectQuery selectQuery7 = new DevExpress.DataAccess.Sql.SelectQuery();
+            DevExpress.DataAccess.Sql.Column column43 = new DevExpress.DataAccess.Sql.Column();
+            DevExpress.DataAccess.Sql.ColumnExpression columnExpression43 = new DevExpress.DataAccess.Sql.ColumnExpression();
+            DevExpress.DataAccess.Sql.Table table13 = new DevExpress.DataAccess.Sql.Table();
+            DevExpress.DataAccess.Sql.Column column44 = new DevExpress.DataAccess.Sql.Column();
+            DevExpress.DataAccess.Sql.ColumnExpression columnExpression44 = new DevExpress.DataAccess.Sql.ColumnExpression();
+            DevExpress.DataAccess.Sql.Column column45 = new DevExpress.DataAccess.Sql.Column();
+            DevExpress.DataAccess.Sql.ColumnExpression columnExpression45 = new DevExpress.DataAccess.Sql.ColumnExpression();
+            DevExpress.DataAccess.Sql.Column column46 = new DevExpress.DataAccess.Sql.Column();
+            DevExpress.DataAccess.Sql.ColumnExpression columnExpression46 = new DevExpress.DataAccess.Sql.ColumnExpression();
+            DevExpress.DataAccess.Sql.Column column47 = new DevExpress.DataAccess.Sql.Column();
+            DevExpress.DataAccess.Sql.ColumnExpression columnExpression47 = new DevExpress.DataAccess.Sql.ColumnExpression();
+            DevExpress.DataAccess.Sql.Column column48 = new DevExpress.DataAccess.Sql.Column();
+            DevExpress.DataAccess.Sql.ColumnExpression columnExpression48 = new DevExpress.DataAccess.Sql.ColumnExpression();
+            DevExpress.DataAccess.Sql.Column column49 = new DevExpress.DataAccess.Sql.Column();
+            DevExpress.DataAccess.Sql.ColumnExpression columnExpression49 = new DevExpress.DataAccess.Sql.ColumnExpression();
+            DevExpress.DataAccess.Sql.Table table14 = new DevExpress.DataAccess.Sql.Table();
+            DevExpress.DataAccess.Sql.Join join7 = new DevExpress.DataAccess.Sql.Join();
+            DevExpress.DataAccess.Sql.RelationColumnInfo relationColumnInfo7 = new DevExpress.DataAccess.Sql.RelationColumnInfo();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.sqlDataSource1 = new DevExpress.DataAccess.Sql.SqlDataSource(this.components);
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colAd = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colSoyad = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colEPosta = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colYas = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colPersonelID = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colTelefon = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colTblDepartman_Ad = new DevExpress.XtraGrid.Columns.GridColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sqlDataSource1
+            // 
+            this.sqlDataSource1.ConnectionName = "localhost_DbPersonelTakipSistemi_Connection";
+            this.sqlDataSource1.Name = "sqlDataSource1";
+            columnExpression43.ColumnName = "Ad";
+            table13.MetaSerializable = "<Meta X=\"30\" Y=\"30\" Width=\"125\" Height=\"243\" />";
+            table13.Name = "TblPersonel";
+            columnExpression43.Table = table13;
+            column43.Expression = columnExpression43;
+            columnExpression44.ColumnName = "Soyad";
+            columnExpression44.Table = table13;
+            column44.Expression = columnExpression44;
+            columnExpression45.ColumnName = "EPosta";
+            columnExpression45.Table = table13;
+            column45.Expression = columnExpression45;
+            columnExpression46.ColumnName = "Yas";
+            columnExpression46.Table = table13;
+            column46.Expression = columnExpression46;
+            columnExpression47.ColumnName = "PersonelID";
+            columnExpression47.Table = table13;
+            column47.Expression = columnExpression47;
+            columnExpression48.ColumnName = "Telefon";
+            columnExpression48.Table = table13;
+            column48.Expression = columnExpression48;
+            column49.Alias = "TblDepartman_Ad";
+            columnExpression49.ColumnName = "Ad";
+            table14.MetaSerializable = "<Meta X=\"185\" Y=\"30\" Width=\"125\" Height=\"103\" />";
+            table14.Name = "TblDepartman";
+            columnExpression49.Table = table14;
+            column49.Expression = columnExpression49;
+            selectQuery7.Columns.Add(column43);
+            selectQuery7.Columns.Add(column44);
+            selectQuery7.Columns.Add(column45);
+            selectQuery7.Columns.Add(column46);
+            selectQuery7.Columns.Add(column47);
+            selectQuery7.Columns.Add(column48);
+            selectQuery7.Columns.Add(column49);
+            selectQuery7.Name = "TblPersonel";
+            relationColumnInfo7.NestedKeyColumn = "DepartmanID";
+            relationColumnInfo7.ParentKeyColumn = "DepartmanKodu";
+            join7.KeyColumns.Add(relationColumnInfo7);
+            join7.Nested = table14;
+            join7.Parent = table13;
+            selectQuery7.Relations.Add(join7);
+            selectQuery7.Tables.Add(table13);
+            selectQuery7.Tables.Add(table14);
+            this.sqlDataSource1.Queries.AddRange(new DevExpress.DataAccess.Sql.SqlQuery[] {
+            selectQuery7});
+            this.sqlDataSource1.ResultSchemaSerializable = resources.GetString("sqlDataSource1.ResultSchemaSerializable");
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.simpleButton1.Location = new System.Drawing.Point(0, 361);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(670, 23);
+            this.simpleButton1.TabIndex = 1;
+            this.simpleButton1.Text = "PDF AKTAR";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.simpleButton2.Location = new System.Drawing.Point(0, 338);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(670, 23);
+            this.simpleButton2.TabIndex = 3;
+            this.simpleButton2.Text = "Execle Aktar";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.DataMember = "TblPersonel";
+            this.gridControl1.DataSource = this.sqlDataSource1;
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 0);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(670, 338);
+            this.gridControl1.TabIndex = 8;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colAd,
+            this.colSoyad,
+            this.colEPosta,
+            this.colYas,
+            this.colPersonelID,
+            this.colTelefon,
+            this.colTblDepartman_Ad});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // colAd
+            // 
+            this.colAd.FieldName = "Ad";
+            this.colAd.Name = "colAd";
+            this.colAd.Visible = true;
+            this.colAd.VisibleIndex = 0;
+            // 
+            // colSoyad
+            // 
+            this.colSoyad.FieldName = "Soyad";
+            this.colSoyad.Name = "colSoyad";
+            this.colSoyad.Visible = true;
+            this.colSoyad.VisibleIndex = 1;
+            // 
+            // colEPosta
+            // 
+            this.colEPosta.FieldName = "EPosta";
+            this.colEPosta.Name = "colEPosta";
+            this.colEPosta.Visible = true;
+            this.colEPosta.VisibleIndex = 2;
+            // 
+            // colYas
+            // 
+            this.colYas.FieldName = "Yas";
+            this.colYas.Name = "colYas";
+            this.colYas.Visible = true;
+            this.colYas.VisibleIndex = 3;
+            // 
+            // colPersonelID
+            // 
+            this.colPersonelID.FieldName = "PersonelID";
+            this.colPersonelID.Name = "colPersonelID";
+            this.colPersonelID.Visible = true;
+            this.colPersonelID.VisibleIndex = 4;
+            // 
+            // colTelefon
+            // 
+            this.colTelefon.FieldName = "Telefon";
+            this.colTelefon.Name = "colTelefon";
+            this.colTelefon.Visible = true;
+            this.colTelefon.VisibleIndex = 5;
+            // 
+            // colTblDepartman_Ad
+            // 
+            this.colTblDepartman_Ad.FieldName = "TblDepartman_Ad";
+            this.colTblDepartman_Ad.Name = "colTblDepartman_Ad";
+            this.colTblDepartman_Ad.Visible = true;
+            this.colTblDepartman_Ad.VisibleIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 384);
+            this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.simpleButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DevExpress.DataAccess.Sql.SqlDataSource sqlDataSource1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraGrid.Columns.GridColumn colAd;
+        private DevExpress.XtraGrid.Columns.GridColumn colSoyad;
+        private DevExpress.XtraGrid.Columns.GridColumn colEPosta;
+        private DevExpress.XtraGrid.Columns.GridColumn colYas;
+        private DevExpress.XtraGrid.Columns.GridColumn colPersonelID;
+        private DevExpress.XtraGrid.Columns.GridColumn colTelefon;
+        private DevExpress.XtraGrid.Columns.GridColumn colTblDepartman_Ad;
+    }
+}
+
---
+++ WizardKullanimi/WizardKullanimi/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WizardKullanimi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.DataAccess.Sql.CustomSqlQuery customSqlQuery1 = new DevExpress.DataAccess.Sql.CustomSqlQuery();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.sqlDataSource1 = new DevExpress.DataAccess.Sql.SqlDataSource(this.components);
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colPersonelID = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colAd = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colSoyad = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colTelefon = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colEPosta = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colYas = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colCinsiyet = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colArsiv = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colTblDepartman_Ad = new DevExpress.XtraGrid.Columns.GridColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.DataMember = "Query";
+            this.gridControl1.DataSource = this.sqlDataSource1;
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 0);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(726, 377);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // sqlDataSource1
+            // 
+            this.sqlDataSource1.ConnectionName = "localhost_DbPersonelTakipSistemi_Connection";
+            this.sqlDataSource1.Name = "sqlDataSource1";
+            customSqlQuery1.Name = "Query";
+            customSqlQuery1.Sql = resources.GetString("customSqlQuery1.Sql");
+            this.sqlDataSource1.Queries.AddRange(new DevExpress.DataAccess.Sql.SqlQuery[] {
+            customSqlQuery1});
+            this.sqlDataSource1.ResultSchemaSerializable = resources.GetString("sqlDataSource1.ResultSchemaSerializable");
+            // 
+            // gridView1
+            // 
+            this.gridView1.Appearance.Row.BackColor = System.Drawing.Color.Tomato;
+            this.gridView1.Appearance.Row.BackColor2 = System.Drawing.Color.LawnGreen;
+            this.gridView1.Appearance.Row.Options.UseBackColor = true;
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colPersonelID,
+            this.colAd,
+            this.colSoyad,
+            this.colTelefon,
+            this.colEPosta,
+            this.colYas,
+            this.colCinsiyet,
+            this.colArsiv,
+            this.colTblDepartman_Ad});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsBehavior.ReadOnly = true;
+            this.gridView1.OptionsView.ShowGroupPanel = false;
+            // 
+            // colPersonelID
+            // 
+            this.colPersonelID.FieldName = "PersonelID";
+            this.colPersonelID.Name = "colPersonelID";
+            this.colPersonelID.Visible = true;
+            this.colPersonelID.VisibleIndex = 0;
+            // 
+            // colAd
+            // 
+            this.colAd.FieldName = "Ad";
+            this.colAd.Name = "colAd";
+            this.colAd.Visible = true;
+            this.colAd.VisibleIndex = 1;
+            // 
+            // colSoyad
+            // 
+            this.colSoyad.FieldName = "Soyad";
+            this.colSoyad.Name = "colSoyad";
+            this.colSoyad.Visible = true;
+            this.colSoyad.VisibleIndex = 2;
+            // 
+            // colTelefon
+            // 
+            this.colTelefon.FieldName = "Telefon";
+            this.colTelefon.Name = "colTelefon";
+            this.colTelefon.Visible = true;
+            this.colTelefon.VisibleIndex = 3;
+            // 
+            // colEPosta
+            // 
+            this.colEPosta.FieldName = "EPosta";
+            this.colEPosta.Name = "colEPosta";
+            this.colEPosta.Visible = true;
+            this.colEPosta.VisibleIndex = 4;
+            // 
+            // colYas
+            // 
+            this.colYas.FieldName = "Yas";
+            this.colYas.Name = "colYas";
+            this.colYas.Visible = true;
+            this.colYas.VisibleIndex = 5;
+            // 
+            // colCinsiyet
+            // 
+            this.colCinsiyet.FieldName = "Cinsiyet";
+            this.colCinsiyet.Name = "colCinsiyet";
+            this.colCinsiyet.Visible = true;
+            this.colCinsiyet.VisibleIndex = 6;
+            // 
+            // colArsiv
+            // 
+            this.colArsiv.FieldName = "Arsiv";
+            this.colArsiv.Name = "colArsiv";
+            this.colArsiv.Visible = true;
+            this.colArsiv.VisibleIndex = 7;
+            // 
+            // colTblDepartman_Ad
+            // 
+            this.colTblDepartman_Ad.FieldName = "TblDepartman_Ad";
+            this.colTblDepartman_Ad.Name = "colTblDepartman_Ad";
+            this.colTblDepartman_Ad.Visible = true;
+            this.colTblDepartman_Ad.VisibleIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 377);
+            this.Controls.Add(this.gridControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.DataAccess.Sql.SqlDataSource sqlDataSource1;
+        private DevExpress.XtraGrid.Columns.GridColumn colPersonelID;
+        private DevExpress.XtraGrid.Columns.GridColumn colAd;
+        private DevExpress.XtraGrid.Columns.GridColumn colSoyad;
+        private DevExpress.XtraGrid.Columns.GridColumn colTelefon;
+        private DevExpress.XtraGrid.Columns.GridColumn colEPosta;
+        private DevExpress.XtraGrid.Columns.GridColumn colYas;
+        private DevExpress.XtraGrid.Columns.GridColumn colCinsiyet;
+        private DevExpress.XtraGrid.Columns.GridColumn colArsiv;
+        private DevExpress.XtraGrid.Columns.GridColumn colTblDepartman_Ad;
+    }
+}
+
