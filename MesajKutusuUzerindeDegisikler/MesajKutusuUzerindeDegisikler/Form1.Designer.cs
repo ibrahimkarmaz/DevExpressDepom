54d036cdc65c94f0cfe7d3628302624ec6b3f00a
İbrahim KARMAZ <107946057+ibrahimkarmaz@users.noreply.github.com>
2022-09-05 23:34:36 +0300

    Mesaj Kutusunu Özelleştirme
    Mesaj Kutusunu süreli veya buttonları resimli vs yapılabilir.
    
    NOT:GELİŞTİRİLMELİ.

---
+++ MesajKutusuUzerindeDegisikler/MesajKutusuUzerindeDegisikler/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace MesajKutusuUzerindeDegisikler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.svgImageCollection1 = new DevExpress.Utils.SvgImageCollection(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.svgImageCollection1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(12, 12);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(439, 50);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "Süreli Mesaj Kutusu";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(12, 68);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(439, 50);
+            this.simpleButton2.TabIndex = 1;
+            this.simpleButton2.Text = "Resimli Mesaj Kutusu";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // svgImageCollection1
+            // 
+            this.svgImageCollection1.Add("lifesaver-svgrepo-com (1)", ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("svgImageCollection1.lifesaver-svgrepo-com (1)"))));
+            this.svgImageCollection1.Add("lifesaver-svgrepo-com", ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("svgImageCollection1.lifesaver-svgrepo-com"))));
+            this.svgImageCollection1.Add("save-icon-svgrepo-com", ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("svgImageCollection1.save-icon-svgrepo-com"))));
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 142);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.simpleButton1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Yeni Mesaj Kutus";
+            ((System.ComponentModel.ISupportInitialize)(this.svgImageCollection1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.Utils.SvgImageCollection svgImageCollection1;
+    }
+}
+
---
+++ MesajKutusuUzerindeDegisikler/MesajKutusuUzerindeDegisikler/Form1.cs
@@ -0,0 +1,77 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MesajKutusuUzerindeDegisikler
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            XtraMessageBoxArgs args = new XtraMessageBoxArgs();
+            args.AutoCloseOptions.Delay = 5000;
+            args.Caption = "Auto-close message";
+            args.Text = "This message closes automatically after 5 seconds.";
+            args.Buttons = new DialogResult[] { DialogResult.OK, DialogResult.Cancel };
+            XtraMessageBox.Show(args).ToString();
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            XtraMessageBoxArgs args = new XtraMessageBoxArgs();
+            args.Caption = "Message";
+            args.Text = "Buttons in this message box show custom images.";
+           // args.Buttons = new DialogResult[] { DialogResult.OK, DialogResult.Cancel, DialogResult.Retry };
+            args.Buttons = new DialogResult[] { DialogResult.OK, DialogResult.Cancel };
+            args.Showing += Args_Showing;
+            DialogResult a= XtraMessageBox.Show(args);
+            if (a==DialogResult.OK)
+            {
+                Console.WriteLine("das");
+            }
+        }
+        private void Args_Showing(object sender, XtraMessageShowingArgs e)
+        {
+            e.Buttons[DialogResult.OK].Text = "Custom OK Text";
+           // e.Buttons[DialogResult.OK].Width = 200;
+            e.Buttons[DialogResult.Cancel].Text = "Custom Cancel Text";
+            //e.Buttons[DialogResult.Cancel].Width = 200;
+            foreach (var control in e.Form.Controls)
+            {
+                SimpleButton button = control as SimpleButton;
+                if (button != null)
+                {
+                    button.ImageOptions.SvgImageSize = new Size(16, 16);
+                     //button.Height = 25;
+                    switch (button.DialogResult.ToString())
+                    {
+                        case ("OK"):
+                            button.ImageOptions.SvgImage = svgImageCollection1[0];
+                            break;
+                        case ("Cancel"):
+                            button.ImageOptions.SvgImage = svgImageCollection1[1];
+                            break;
+                      /*  case ("Retry"):
+                            button.ImageOptions.SvgImage = svgImageCollection1[2];
+                            break;*/
+                    }
+                }
+            }
+        }
+        //KAYNAKÇA
+        //https://docs.devexpress.com/WindowsForms/114566/controls-and-libraries/messages-notifications-and-dialogs/xtramessagebox
+        //NOT 1:BUTONLARI ORTALAMAK İÇİN YÖNTEM VAR FAKAT KULLANILAMADI(ARAŞTIR)
+        //NOT 2:BUTTONLARIN BOYUTLARI AYARLANABİLİYOR KAPATILDI BAKILABİLİR.
+    }
+}
